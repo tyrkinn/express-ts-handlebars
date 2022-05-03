c74f903661d635a8b6f19954d9a00603af07534a
tyrkinn <nikemad61@gmail.com>
2022-05-04 01:16:11 +0300

    Add handlebars and config express app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import express from 'express';
-import path from 'path';
+import { create } from 'express-handlebars';
+import cors from 'cors';
 
 const app = express();
+
+const hbsConfig = create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.engine('hbs', hbsConfig.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.get('/', (_, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index');
 });
 
 export default app;
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import app from './app';
 import logger from './logger';
 
 const PORT = process.env.PORT ?? 3000;
-
-app.listen(PORT, () => {
-  logger.info(`Server started at http://localhost:${PORT}`);
-});
+try {
+  app.listen(PORT, () => {
+    logger.info(`Server started at http://localhost:${PORT}`);
+  });
+} catch (e: unknown) {
+  logger.error((e as Error).message);
+}
